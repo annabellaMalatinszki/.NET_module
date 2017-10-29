9931e0f3b6a5e2f24d077772ee453d864d71f261
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-10-29 21:10:54 +0100

    Complete Exercise 2

--- CreateClass/CreateClass/Program.cs
+++ CreateClass/CreateClass/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; 
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +10,16 @@ namespace CreateClass
     {
         static void Main(string[] args)
         {
-            Person person = new Person("John Miller", 19890101, Genders.Male);
+            Person person = new Person("Donald Trump", 19490101, Genders.Male);
             System.Console.WriteLine(person.ToString());
+
+            Room room1 = new Room(23);
+            Person employee1 = new Employee("Sarah Huckabee Sanders", 19710101, Genders.Female, 20000, "Spokesperson", room1);
+            System.Console.WriteLine(employee1.ToString());
+
+            Room room2 = new Room(21);
+            Person employee2 = new Employee("John Kelly", 19510101, Genders.Male, 30000, "General", room2);
+            System.Console.WriteLine(employee2.ToString());
 
             // Keep the console window open in debug mode.
             System.Console.WriteLine("Press any key to exit.");
@@ -20,21 +28,60 @@ namespace CreateClass
     }
     public class Person
     {
-        private string Name;
-        private double BirthDate;
-        private Genders Gender;
+        public string name;
+        public int birthDate;
+        public Genders gender;
 
-        public Person(String Name, double BirthDate, Genders Gender)
+        public Person(String name, int birthDate, Genders gender)
         {
-            this.Name = Name;
-            this.BirthDate = BirthDate;
-            this.Gender = Gender;
+            this.name = name;
+            this.birthDate = birthDate;
+            this.gender = gender;
         }
 
         public override string ToString()
         {
-            string StringToPrint = Name + " was born on " + BirthDate + " and is a " + Gender;
+            string StringToPrint = name + " was born on " + birthDate + " and is a " + gender;
             return StringToPrint;
+        } 
+    }
+
+    public class Employee : Person
+    {
+        private double salary;
+        private string profession;
+        private Room room;
+
+        public Employee(string name, int birthDate, Genders gender, double salary, string profession, Room room)
+            : base(name, birthDate, gender)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+            this.gender = gender;
+            this.salary = salary;
+            this.profession = profession;
+            this.room = room;
+        }
+
+        public override string ToString()
+        {
+            string StringToPrint = name + ", who is a " + gender + ", works as a " + profession + " for " + salary + "$ a month and was born on " + birthDate + ". Works in room " + room.GetRoomNumber(); 
+            return StringToPrint;
+        }
+    }
+
+    public class Room
+    {
+        private int roomNumber;
+
+        public Room(int roomNumber) 
+        {
+            this.roomNumber = roomNumber;
+        }
+
+        public int GetRoomNumber() 
+        {
+            return roomNumber;
         }
     }
 
