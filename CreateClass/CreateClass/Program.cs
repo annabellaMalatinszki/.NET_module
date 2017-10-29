da21170d1fd7f4b00da297fd9e37fca3008dd7a4
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-10-29 19:46:52 +0100

    Complete Exercise 1 by creating Person class

---
+++ CreateClass/CreateClass/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("John Miller", 19890101, Genders.Male);
+            System.Console.WriteLine(person.ToString());
+
+            // Keep the console window open in debug mode.
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+    public class Person
+    {
+        private string Name;
+        private double BirthDate;
+        private Genders Gender;
+
+        public Person(String Name, double BirthDate, Genders Gender)
+        {
+            this.Name = Name;
+            this.BirthDate = BirthDate;
+            this.Gender = Gender;
+        }
+
+        public override string ToString()
+        {
+            string StringToPrint = Name + " was born on " + BirthDate + " and is a " + Gender;
+            return StringToPrint;
+        }
+    }
+
+    public enum Genders
+    {
+        Male,
+        Female
+    }
+}
