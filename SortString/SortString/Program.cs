ba51b7fea64d70eef48e773a1cfd82090d50d50b
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-10-29 22:04:34 +0100

    Complete Exercise 5

---
+++ SortString/SortString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "'Donald Trump is a waddling outhouse' is a quote from Bill Maher";
+            string[] words = str.Split(' ');
+
+            Array.Sort(words);
+
+            String s = string.Join(" ", words);
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+    }
+}
