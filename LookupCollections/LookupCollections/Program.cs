c00efbbb3f30f26f0c71a7808f31c515689fa723
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-11-08 16:09:11 +0100

    Exercise1/Exercise4

---
+++ LookupCollections/LookupCollections/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+
+
+
+namespace LookupCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDictionary listDictionary = new ListDictionary(StringComparer.InvariantCultureIgnoreCase);
+            listDictionary.Add("Estados Unidos", "United States");
+            listDictionary.Add("Canadá", "Canada");
+            listDictionary.Add("España", "Spain");
+
+            Console.WriteLine(listDictionary["españa"]);
+            Console.WriteLine(listDictionary["canaDÁ"]);
+
+            Console.ReadKey();
+        }
+    }
+}
