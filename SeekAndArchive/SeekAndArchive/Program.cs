40454655bd113a9ed0dcc1c83e2905cf81509706
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-11-12 20:06:35 +0100

    Advanced Task/Exercise 1

--- SeekAndArchive/SeekAndArchive/Program.cs
+++ SeekAndArchive/SeekAndArchive/Program.cs
@@ -3,13 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace SeekAndArchive
 {
     class Program
     {
+        static List<FileInfo> FoundFiles;
         static void Main(string[] args)
         {
+           string fileName = args[0];
+           string directoryName = args[1];
+
+            FoundFiles = new List<FileInfo>();
+            DirectoryInfo rootDir = new DirectoryInfo(directoryName);
+            if (!rootDir.Exists)
+            {
+                Console.WriteLine("The specified directory does not exist.");
+                return;
+            }
+
+            RecursiveSearch(FoundFiles, fileName, rootDir);
+
+            Console.WriteLine("Found {0} files.", FoundFiles.Count());
+            foreach (FileInfo file in FoundFiles)
+            {
+                Console.WriteLine("{0}", file.FullName);
+            }
+            Console.ReadKey();
+        }
+
+        static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
+        {
+            foreach (FileInfo file in currentDirectory.GetFiles())
+
+            {
+                if (new Regex("^" + Regex.Escape(fileName).Replace(@"\*", ".*").Replace(@"\?", ".") + "$", RegexOptions.IgnoreCase | RegexOptions.Singleline).IsMatch(file.Name))
+                {
+                    foundFiles.Add(file);
+                }
+            }
+            
+            foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
+            {
+                RecursiveSearch(foundFiles, fileName, dir);
+            }
         }
     }
 }
