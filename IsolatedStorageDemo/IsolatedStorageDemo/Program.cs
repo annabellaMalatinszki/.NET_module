0d0aed72d1556b6c6e2280b06ea9c3f6d9cada7c
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-11-13 00:16:26 +0100

    Advanced task/Exercise 5

--- IsolatedStorageDemo/IsolatedStorageDemo/Program.cs
+++ IsolatedStorageDemo/IsolatedStorageDemo/Program.cs
@@ -21,13 +21,26 @@ namespace IsolatedStorageDemo
                 StreamWriter userWriter = new StreamWriter(userStream);
                 userWriter.WriteLine("User Prefs");
                 userWriter.Close();
+
+                string[] files = userStore.GetFileNames("UserSettings.set");
+                if (files.Length == 0)
+                {
+                    Console.WriteLine("No files found");
+                }
+                else
+                {
+                    userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore);
+                    StreamReader userReader = new StreamReader(userStream);
+                    string contents = userReader.ReadToEnd();
+                    Console.WriteLine(contents);
+                    Console.ReadKey();
+                }
             }
             catch(FileNotFoundException e)
             {
                 Console.WriteLine(e.Message);
                 Console.ReadKey();
             }
-
         }
     }
 }
