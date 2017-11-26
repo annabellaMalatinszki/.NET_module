57a0f5e908083b79c2ede88edadc1f2337dd9c33
annabellaMalatinszki <a.malatinszki@gmail.com>
2017-11-27 00:30:57 +0100

    Regular Expressions/Exercise3

---
+++ UTF7Converter/UTF7Converter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UTF7Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\amala\Desktop\q.txt");
+            StreamWriter sw = new StreamWriter(@"C:\Users\amala\Desktop\q-utf7.txt", false, Encoding.UTF7);
+
+            sw.WriteLine(sr.ReadToEnd());
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
